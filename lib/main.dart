0bef36a159d8e09885a467b309949917b56df8b1
aman <amansingh08087@gmail.com>
2025-09-26 00:25:50 +0530

    d

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_roadmap/family_builder_provider/home_screen.dart';
-import 'package:flutter_roadmap/get_api_riverpod/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
